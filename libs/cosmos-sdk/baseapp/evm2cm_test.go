2f62756ae4c64ed51d3de5b7e8b9667370442271
xingqiang.yuan <xingqiang.yuan@okg.com>
2023-03-08 09:54:16 +0800

    change to okbchain

--- libs/cosmos-sdk/baseapp/evm2cm_test.go
+++ libs/cosmos-sdk/baseapp/evm2cm_test.go
@@ -186,7 +186,7 @@ func TestParseMsgWrapper(t *testing.T) {
 			input: `{"type": "okbchain/staking/MsgDeposit","value": {"delegator_address": "0x4375D630687C83471829227b5C1Ea92217FD6265","quantity": {"denom": "okb","amount": "1"}}}`,
 			fnCheck: func(ret *MsgWrapper, err error) {
 				require.NoError(t, err)
-				require.Equal(t, "okexchain/staking/MsgDeposit", ret.Name)
+				require.Equal(t, "okbchain/staking/MsgDeposit", ret.Name)
 				require.Equal(t, "{\"delegator_address\": \"0x4375D630687C83471829227b5C1Ea92217FD6265\",\"quantity\": {\"denom\": \"okb\",\"amount\": \"1\"}}", string(ret.Data))
 			},
 		},
@@ -194,7 +194,7 @@ func TestParseMsgWrapper(t *testing.T) {
 			input: `{"type": "okbchain/staking/MsgWithdraw","value": {"delegator_address": "0x4375D630687C83471829227b5C1Ea92217FD6265","quantity": {"denom": "okb","amount": "1"}}}`,
 			fnCheck: func(ret *MsgWrapper, err error) {
 				require.NoError(t, err)
-				require.Equal(t, "okexchain/staking/MsgWithdraw", ret.Name)
+				require.Equal(t, "okbchain/staking/MsgWithdraw", ret.Name)
 				require.Equal(t, "{\"delegator_address\": \"0x4375D630687C83471829227b5C1Ea92217FD6265\",\"quantity\": {\"denom\": \"okb\",\"amount\": \"1\"}}", string(ret.Data))
 			},
 		},
