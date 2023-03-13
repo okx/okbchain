805d326429a2e200cb83c64a6300291903be7170
KamiD <44460798+KamiD@users.noreply.github.com>
2023-03-13 09:21:21 +0800

    Merge PR: change mint to zero (#18)
    * change deflation rate to zero
    
    * change mint to 0
    
    * fix ut
    
    * update
    
    * add ut
    
    * add ut
    
    * optimized code
    
    * fix ut
    
    * fix ut
    
    * fix cli display
    
    * rename
    
    * rename
    
    * fix ut
    
    * modify call function about handle extra proposal
    
    ---------
    
    Co-authored-by: Zhong Qiu <36867992+zhongqiuwood@users.noreply.github.com>
    Co-authored-by: jianguo <zjg555543@163.com>

--- libs/ibc-go/testing/simapp/app.go
+++ libs/ibc-go/testing/simapp/app.go
@@ -152,6 +152,7 @@ var (
 			evmclient.ManageSysContractAddressProposalHandler,
 			evmclient.ManageContractByteCodeProposalHandler,
 			govclient.ManageTreasuresProposalHandler,
+			govclient.ExtraProposalHandler,
 			erc20client.TokenMappingProposalHandler,
 			erc20client.ProxyContractRedirectHandler,
 			wasmclient.MigrateContractProposalHandler,
