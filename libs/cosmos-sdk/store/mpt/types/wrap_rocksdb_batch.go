b18dfcb9e5d9fa8ce151621c4790ea897d219414
cwbhhjl <cwbhhjl@gmail.com>
2023-03-17 18:20:56 +0800

    use Finalizer to close rocksdb batch

--- libs/cosmos-sdk/store/mpt/types/wrap_rocksdb_batch.go
+++ libs/cosmos-sdk/store/mpt/types/wrap_rocksdb_batch.go
@@ -4,84 +4,72 @@
 package types
 
 import (
+	"runtime"
+
+	"github.com/cosmos/gorocksdb"
 	"github.com/ethereum/go-ethereum/ethdb"
 	tmdb "github.com/okx/okbchain/libs/tm-db"
 )
 
 var _ ethdb.Batch = (*WrapRocksDBBatch)(nil)
 
-type record struct {
-	key   []byte
-	value []byte
-}
-
 type WrapRocksDBBatch struct {
-	db      *tmdb.RocksDB
-	records []record
-	size    int
+	batch     *tmdb.RocksDBBatch
+	valueSize int
 }
 
 func NewWrapRocksDBBatch(db *tmdb.RocksDB) *WrapRocksDBBatch {
-	return &WrapRocksDBBatch{db: db}
+	batch := tmdb.NewRocksDBBatch(db)
+	runtime.SetFinalizer(batch, func(b *tmdb.RocksDBBatch) {
+		if b == nil {
+			return
+		}
+		b.Close()
+		runtime.SetFinalizer(b, nil)
+	})
+	return &WrapRocksDBBatch{
+		batch: batch,
+	}
 }
 
 func (wrb *WrapRocksDBBatch) Put(key []byte, value []byte) error {
-	wrb.records = append(wrb.records, record{
-		key:   key,
-		value: nonNilBytes(value),
-	})
-	wrb.size += len(value)
+	wrb.batch.Set(key, value)
+	wrb.valueSize += len(key) + len(value)
 	return nil
 }
 
 func (wrb *WrapRocksDBBatch) Delete(key []byte) error {
-	wrb.records = append(wrb.records, record{
-		key: key,
-	})
-	wrb.size += len(key)
+	wrb.batch.Delete(key)
+	wrb.valueSize += len(key)
 	return nil
 }
 
 func (wrb *WrapRocksDBBatch) ValueSize() int {
-	return wrb.size
+	return wrb.valueSize
 }
 
 func (wrb *WrapRocksDBBatch) Write() error {
-	batch := tmdb.NewRocksDBBatch(wrb.db)
-	for _, rcd := range wrb.records {
-		if rcd.value != nil {
-			batch.Set(rcd.key, rcd.value)
-		} else {
-			batch.Delete(rcd.key)
-		}
-	}
-
-	return batch.Write()
+	return wrb.batch.WriteWithoutClose()
 }
 
 // Replay replays the batch contents.
 func (wrb *WrapRocksDBBatch) Replay(w ethdb.KeyValueWriter) error {
-	var err error
-	for _, rcd := range wrb.records {
-		if rcd.value != nil {
-			err = w.Put(rcd.key, rcd.value)
-		} else {
-			err = w.Delete(rcd.key)
-		}
-		if err != nil {
-			return err
+	iter := wrb.batch.NewIterator()
+	for iter.Next() {
+		switch rcd := iter.Record(); rcd.Type {
+		case gorocksdb.WriteBatchValueRecord:
+			if err := w.Put(rcd.Key, rcd.Value); err != nil {
+				return err
+			}
+		case gorocksdb.WriteBatchDeletionRecord:
+			if err := w.Delete(rcd.Key); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
 }
 
 func (wrb *WrapRocksDBBatch) Reset() {
-	wrb.records = wrb.records[:0]
-}
-
-func nonNilBytes(bz []byte) []byte {
-	if bz == nil {
-		return []byte{}
-	}
-	return bz
+	wrb.batch.Reset()
 }
