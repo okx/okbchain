9682560ba91c14d598e1bbd1735d8b76104e9c33
zhangkai <zhangkai.gis@163.com>
2023-04-05 21:12:50 +0800

    add unittest

---
+++ libs/cosmos-sdk/store/mpt/snapshot_test.go
@@ -0,0 +1,46 @@
+package mpt
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/prque"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/ethdb/memorydb"
+	tmlog "github.com/okx/okbchain/libs/tendermint/libs/log"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestMptStore_openSnapshot(t *testing.T) {
+	db := memorydb.New()
+	ethDb := rawdb.NewDatabase(db)
+	stateDb := state.NewDatabase(ethDb)
+
+	trie, err := stateDb.OpenTrie(common.Hash{})
+	require.NoError(t, err)
+
+	mptStore := &MptStore{
+		trie:         trie,
+		db:           stateDb,
+		logger:       tmlog.NewNopLogger(),
+		originalRoot: trie.Hash(),
+		retriever:    EmptyStateRootRetriever{},
+		triegc:       prque.New(nil),
+	}
+	SetSnapshotRebuild(true)
+	err = mptStore.openSnapshot()
+	require.NoError(t, err)
+	SetSnapshotRebuild(false)
+	err = mptStore.openSnapshot()
+	require.NoError(t, err)
+	addr := AddressStoreKey(common.Hash{1}.Bytes())
+	value := "value1"
+	addr2 := AddressStoreKey(common.Hash{2}.Bytes())
+	value2 := "value2"
+	mptStore.Set(addr, []byte(value))
+	mptStore.Set(addr2, []byte(value2))
+	mptStore.Delete(addr)
+	mptStore.CommitterCommit(nil)
+	v := mptStore.Get(addr)
+	require.Nil(t, v)
+}
