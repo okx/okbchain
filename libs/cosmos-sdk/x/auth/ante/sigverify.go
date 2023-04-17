cb1179145d26fb78c87bde17f778c48a4fcd5cfb
jianguo <zjg555543@163.com>
2023-04-17 19:17:48 +0800

    3050

--- libs/cosmos-sdk/x/auth/ante/sigverify.go
+++ libs/cosmos-sdk/x/auth/ante/sigverify.go
@@ -3,9 +3,9 @@ package ante
 import (
 	"bytes"
 	"encoding/hex"
+	"github.com/okx/okbchain/app/crypto/ethsecp256k1"
 	"github.com/okx/okbchain/libs/tendermint/crypto/etherhash"
 
-	"github.com/okx/okbchain/app/crypto/ethsecp256k1"
 	"github.com/okx/okbchain/libs/cosmos-sdk/codec"
 	sdk "github.com/okx/okbchain/libs/cosmos-sdk/types"
 	sdkerrors "github.com/okx/okbchain/libs/cosmos-sdk/types/errors"
@@ -82,33 +82,22 @@ func (spkd SetPubKeyDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate b
 		}
 
 		// Only make check if simulate=false
-		if !simulate && !bytes.Equal(pk.Address(), signers[i]) {
-			switch ppk := pk.(type) {
-			case *secp256k1.PubKeySecp256k1:
-				// In case that tx is created by CosmWasmJS with pubKey type of `secp256k1`
-				// 	and the signer address is derived by the pubKey of `ethsecp256k1` type.
-				// Let it pass after Earth height.
-				if types2.HigherThanEarth(ctx.BlockHeight()) && bytes.Equal(ethsecp256k1.PubKey(ppk[:]).Address(), signers[i]) {
-					break
-				}
-				return ctx, sdkerrors.Wrapf(sdkerrors.ErrInvalidPubKey,
-					"pubKey does not match signer address %s derived by eth pubKey, with signer index: %d", signers[i], i)
-
-			default:
-				//old logic
+		var valid bool
+		if !simulate {
+			if pk, valid = checkSigner(pk, signers[i], ctx.BlockHeight()); !valid {
 				return ctx, sdkerrors.Wrapf(sdkerrors.ErrInvalidPubKey,
 					"pubKey does not match signer address %s with signer index: %d", signers[i], i)
 			}
 		}
-
 		acc, err := GetSignerAcc(ctx, spkd.ak, signers[i])
 		if err != nil {
 			return ctx, err
 		}
 		// account already has pubkey set,no need to reset
-		if acc.GetPubKey() != nil {
+		if !isPubKeyNeedChange(acc.GetPubKey(), pk, ctx.BlockHeight()) {
 			continue
 		}
+		err = acc.SetPubKey(pk)
 		err = acc.SetPubKey(pk)
 		if err != nil {
 			return ctx, sdkerrors.Wrap(sdkerrors.ErrInvalidPubKey, err.Error())
@@ -117,6 +106,38 @@ func (spkd SetPubKeyDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate b
 	}
 
 	return next(ctx, tx, simulate)
+}
+
+func checkSigner(pk crypto.PubKey, signer sdk.AccAddress, height int64) (crypto.PubKey, bool) {
+	if bytes.Equal(pk.Address(), signer) {
+		return pk, true
+	}
+	// In case that tx is created by CosmWasmJS with pubKey type of `secp256k1`
+	// 	and the signer address is derived by the pubKey of `ethsecp256k1` type.
+	// Let it pass after Earth height.
+	if types2.HigherThanEarth(height) {
+		switch v := pk.(type) {
+		case secp256k1.PubKeySecp256k1:
+			ethPub := ethsecp256k1.PubKey(v[:])
+			return ethPub, bytes.Equal(ethPub.Address(), signer)
+		case *secp256k1.PubKeySecp256k1:
+			ethPub := ethsecp256k1.PubKey(v[:])
+			return ethPub, bytes.Equal(ethPub.Address(), signer)
+		}
+	}
+	return pk, false
+}
+
+func isPubKeyNeedChange(pk1, pk2 crypto.PubKey, height int64) bool {
+	if pk1 == nil {
+		return true
+	}
+	if !types2.HigherThanEarth(height) {
+		return false
+	}
+
+	// check if two public keys are equal
+	return pk1.Equals(pk2)
 }
 
 // Consume parameter-defined amount of gas for each signature according to the passed-in SignatureVerificationGasConsumer function
