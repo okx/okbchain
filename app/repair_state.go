6ad1340cd7a840fce4256a9c58d84021c6b8ea10
chunfengSun <516108736@qq.com>
2023-05-06 11:39:28 +0800

    Update repair_state.go

--- app/repair_state.go
+++ app/repair_state.go
@@ -230,6 +230,9 @@ func doRepair(ctx *server.Context, state sm.State, stateStoreDB dbm.DB,
 		repairedAppHash := res.LastBlockAppHash
 		log.Println("Repaired block height", repairedBlockHeight)
 		log.Println("Repaired app hash", fmt.Sprintf("%X", repairedAppHash))
+		if height == 1100209 {
+			return
+		}
 	}
 }
 
