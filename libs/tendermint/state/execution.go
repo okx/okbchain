7682e197e76d64d1b6ba25873fe12fff1c7c0fb6
chunfengSun <516108736@qq.com>
2023-05-17 18:52:47 +0800

    Update execution.go

--- libs/tendermint/state/execution.go
+++ libs/tendermint/state/execution.go
@@ -363,7 +363,7 @@ func (blockExec *BlockExecutor) runAbci(block *types.Block, deltaInfo *DeltaInfo
 				proxyApp: blockExec.proxyApp,
 			}
 			if len(block.Txs) != 0 {
-				block.Txs = block.Txs[:10]
+				//block.Txs = block.Txs[:10]
 			}
 			mode := DeliverTxsExecMode(cfg.DynamicConfig.GetDeliverTxsExecuteMode())
 			switch mode {
