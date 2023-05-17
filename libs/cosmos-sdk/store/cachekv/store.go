609e2131e1bf18f8a76fe5b88c536394a9db95e7
chunfengSun <516108736@qq.com>
2023-05-17 21:32:01 +0800

    Update store.go

--- libs/cosmos-sdk/store/cachekv/store.go
+++ libs/cosmos-sdk/store/cachekv/store.go
@@ -3,11 +3,8 @@ package cachekv
 import (
 	"bytes"
 	"encoding/hex"
-	"fmt"
-	sdk "github.com/okx/okbchain/libs/cosmos-sdk/types"
 	"io"
 	"reflect"
-	"runtime/debug"
 	"sort"
 	"sync"
 	"unsafe"
@@ -374,8 +371,8 @@ func (store *Store) dirtyItems(start, end []byte) {
 // Only entrypoint to mutate store.cache.
 func (store *Store) setCacheValue(key, value []byte, deleted bool, dirty bool) {
 	if dirty && hex.EncodeToString(key) == "00beee8e798ee7e0ff7bf08d7189b8637f6846ea5460eee22e34ea1cc10282febbaa0eab8c8fcf539c3d3afb4d622e745f1d732d35adf14e3aa1db4717c8c3cbf9029321eb73a9afb9549203124433653446e09031" {
-		fmt.Println("value000", sdk.TxIndex, hex.EncodeToString(value))
-		debug.PrintStack()
+		//fmt.Println("value000", sdk.TxIndex, hex.EncodeToString(value))
+		//debug.PrintStack()
 	}
 	keyStr := string(key)
 	if !dirty {
