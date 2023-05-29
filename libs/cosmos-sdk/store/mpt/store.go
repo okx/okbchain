68238953e0fc74a1c4607991472b164618bf495a
zhangkai <zhangkai.gis@163.com>
2023-05-29 10:49:55 +0800

    add logs

--- libs/cosmos-sdk/store/mpt/store.go
+++ libs/cosmos-sdk/store/mpt/store.go
@@ -1,10 +1,12 @@
 package mpt
 
 import (
+	"bytes"
 	"encoding/hex"
 	"fmt"
 	"github.com/okx/okbchain/libs/system/trace/persist"
 	"io"
+	"log"
 	"sync"
 	"time"
 
@@ -342,6 +344,9 @@ func (ms *MptStore) commitStorageWithDelta(storageDelta []*trie.StorageDelta, no
 	}
 	for _, storage := range storageDelta {
 		addr := storage.Addr
+		if bytes.Equal(addr[:], ethcmn.Hex2Bytes("0x6428691F926840bf2C123bB2c281B9aF980f52a3")) {
+			log.Println("dds--------commitStorageWithDelta", addr.String())
+		}
 		addrHash := mpttype.Keccak256HashWithSyncPool(addr[:])
 		stateRoot := ms.retriever.GetAccStateRoot(storage.PreAcc)
 		t, err := ms.db.OpenStorageTrie(addrHash, stateRoot)
@@ -389,6 +394,9 @@ func (ms *MptStore) commitStorageForDelta(nodeSets *trie.MergedNodeSet) []*trie.
 
 func (ms *MptStore) commitStorage(nodeSets *trie.MergedNodeSet) {
 	for addr, v := range ms.storageTrieForWrite {
+		if bytes.Equal(addr[:], ethcmn.Hex2Bytes("0x6428691F926840bf2C123bB2c281B9aF980f52a3")) {
+			log.Println("normal------commitStorage", addr.String())
+		}
 		stateR, set, err := v.Commit(false)
 		if err != nil {
 			panic(fmt.Errorf("unexcepted err:%v while commit storage tire ", err))
