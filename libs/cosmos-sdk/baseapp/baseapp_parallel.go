ec90ef18edbb9f3b6d40f2350090459c1ce0567c
Leo <335209779@qq.com>
2023-06-14 16:24:46 +0800

    fix smb caused by e2c/InstantiateContract

--- libs/cosmos-sdk/baseapp/baseapp_parallel.go
+++ libs/cosmos-sdk/baseapp/baseapp_parallel.go
@@ -263,6 +263,11 @@ func (app *BaseApp) runTxs() []*abci.ResponseDeliverTx {
 			pm.blockGasMeterMu.Unlock()
 
 			pm.SetCurrentIndex(pm.upComingTxIndex, res)
+
+			if !res.msIsNil {
+				pm.currTxFee = pm.currTxFee.Add(pm.extraTxsInfo[pm.upComingTxIndex].fee.Sub(pm.finalResult[pm.upComingTxIndex].paraMsg.RefundFee)...)
+			}
+
 			currentGas += uint64(res.resp.GasUsed)
 
 			if isReRun {
@@ -703,6 +708,10 @@ var (
 func (pm *parallelTxManager) isConflict(e *executeResult) bool {
 	if e.msIsNil {
 		return true //TODO fix later
+	}
+
+	if e.paraMsg.InvalidExecute {
+		return true
 	}
 	for storeKey, rw := range e.rwSet {
 		delete(rw.Read, string(feeAccountKey))
