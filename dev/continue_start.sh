59dd8288a1a63c3d901ee5c9b33ce4a65a11ba65
KeFine <root@ip-172-31-31-207.us-west-2.compute.internal>
2023-07-03 12:09:22 +0000

    for onenet test

---
+++ dev/continue_start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+okbchaind start --rpc.unsafe --local-rpc-port 26657 --log_level "main:info,iavl:info,*:error,state:info,provider:info" --log_file json --dynamic-gp-mode=2 --consensus.timeout_commit 2000ms --enable-preruntx=1 --tree-enable-async-commit=false --enable-gid --fast-query=true --append-pid=true --iavl-output-modules evm=0,acc=0 --commit-gap-height 3 --trie.dirty-disabled=true --trace --home ./_cache_evm --chain-id okbchain-67 --elapsed Round=1,CommitRound=1,Produce=1
