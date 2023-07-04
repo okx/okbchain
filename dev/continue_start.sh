ad21c66dc850003658373635492b9a0801d99d7c
KeFine <root@ip-172-31-31-207.us-west-2.compute.internal>
2023-07-04 04:01:43 +0000

    test onenet

--- dev/continue_start.sh
+++ dev/continue_start.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-okbchaind start --rpc.unsafe --local-rpc-port 26657 --log_level "main:info,iavl:info,*:error,state:info,provider:info" --log_file json --dynamic-gp-mode=2 --consensus.timeout_commit 2000ms --enable-preruntx=1 --tree-enable-async-commit=false --enable-gid --fast-query=true --append-pid=true --iavl-output-modules evm=0,acc=0 --commit-gap-height 3 --trie.dirty-disabled=true --trace --home ./_cache_evm --chain-id okbchain-67 --elapsed Round=1,CommitRound=1,Produce=1
+nohup okbchaind start \
+	--rpc.unsafe \
+	--local-rpc-port 26657 \
+	--log_level "main:info,*:error" \
+	--log_file json \
+	--dynamic-gp-mode=2 \
+	--consensus.timeout_commit 3.5s \
+	--enable-preruntx=1 \
+	--enable-gid \
+	--fast-query=false \
+	--append-pid=true \
+	--iavl-output-modules evm=0,acc=0 \
+	--trie.dirty-disabled=true \
+	--trace --home ./_cache_evm \
+	--chain-id okbchain-67 \
+	--elapsed Round=1,CommitRound=1,Produce=1 \
+	--deliver-txs-mode=2 \
+	--mempool.max_tx_num_per_block=20000 \
+	--mempool.size=200000 \
+	--tree-enable-async-commit=false \
+	--commit-gap-height 3 \
+	&
