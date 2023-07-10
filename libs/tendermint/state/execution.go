5c47acdfc1053eda5380d2745574208aeb83954b
chengzhinei <chengxue5yuan@163.com>
2023-07-10 12:20:34 +0800

    log

--- libs/tendermint/state/execution.go
+++ libs/tendermint/state/execution.go
@@ -2,6 +2,7 @@ package state
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 	"time"
 
@@ -204,6 +205,7 @@ func (blockExec *BlockExecutor) ValidateBlock(state State, block *types.Block) e
 // It takes a blockID to avoid recomputing the parts hash.
 func (blockExec *BlockExecutor) ApplyBlock(
 	state State, blockID types.BlockID, block *types.Block) (State, int64, error) {
+	log.Println("=========ApplyBlock")
 	if ApplyBlockPprofTime >= 0 {
 		f, t := PprofStart()
 		defer PprofEnd(int(block.Height), f, t)
