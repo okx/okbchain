537a545eaf3f19d83192454c3c911d199b1ebf10
chengzhinei <chengxue5yuan@163.com>
2023-07-11 12:07:35 +0800

    rm log

--- libs/cosmos-sdk/store/rootmulti/rootmulti_store.go
+++ libs/cosmos-sdk/store/rootmulti/rootmulti_store.go
@@ -1181,7 +1181,6 @@ func commitStores(version int64, storeMap map[types.StoreKey]types.CommitKVStore
 		mpt.UpdateCommitGapHeight(gap)
 	}
 	for key, store := range storeMap {
-		fmt.Println("commit-key:", key.Name())
 		if isUseless(key.Name(), version, store, filters) {
 			continue
 		}
