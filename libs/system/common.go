aa016cabf9a9831def3b181b081d71dd6ef0f67a
LeoGuo621 <335209779@qq.com>
2023-08-31 15:07:41 +0800

    Update common.go
    update

--- libs/system/common.go
+++ libs/system/common.go
@@ -1,15 +1,15 @@
 package system
 
 const (
-	Chain = "okbchain"
-	TestnetPrefix = "okbchaintest"
-	AppName = "OKBChain"
-	Server = Chain+"d"
-	Client = Chain+"cli"
-	ServerHome = "$HOME/."+Server
-	ClientHome = "$HOME/."+Client
-	ServerLog = Server+".log"
-	EnvPrefix = "OKBCHAIN"
-	CoinType uint32 = 60
-	Currency = "okb"
+	Chain                = "brczero"
+	TestnetPrefix        = "brczerotest"
+	AppName              = "BRCZero"
+	Server               = Chain + "d"
+	Client               = Chain + "cli"
+	ServerHome           = "$HOME/." + Server
+	ClientHome           = "$HOME/." + Client
+	ServerLog            = Server + ".log"
+	EnvPrefix            = "BRCZero"
+	CoinType      uint32 = 60
+	Currency             = "brczero"
 )
